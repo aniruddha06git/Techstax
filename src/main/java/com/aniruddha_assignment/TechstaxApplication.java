ab985eb4334097ebb47976ada1c88b3bca06fede
Aniruddha <aniruddhakamble805@gmail.com>
2023-02-26 10:01:56 +0530

    code

---
+++ src/main/java/com/aniruddha_assignment/TechstaxApplication.java
@@ -0,0 +1,13 @@
+package com.aniruddha_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstaxApplication.class, args);
+	}
+
+}
